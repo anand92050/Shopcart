54683c3ccdb4eb7759fb2acee555a05ee4d1b52e
anand92050 <anandrajrpsraj3236@gmail.com>
2024-06-06 11:03:49 +0530

    added dotenv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
+const { PORT } = require('./config/serverConfig');
+
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log("server started!!");
+app.listen(3000, () =>{
+    console.log(`server started!! at ${PORT}`);
 })
